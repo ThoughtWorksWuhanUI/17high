1f466318a921996c46c3a865abe3ceebea803da3
benweizhu <benweizhu@gmail.com>
2017-02-05 09:40:01 +0800

    vendor css - revert unnecessary change

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import DemoComponent from './components/DemoComponent';
 
-
 const App = () => <DemoComponent />;
 
 render(<App />, document.getElementById('app'));
