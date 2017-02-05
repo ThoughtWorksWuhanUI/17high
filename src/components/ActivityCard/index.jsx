0859cc42388d198565714d78aa136eb115b1f50e
benweizhu <benweizhu@gmail.com>
2017-02-05 14:14:00 +0800

    activity card - add image placeholder

--- src/components/ActivityCard/index.jsx
+++ src/components/ActivityCard/index.jsx
@@ -4,21 +4,28 @@ import classNames from 'classnames';
 import { smartMoment } from '../../utils/moment';
 import styles, { container, image, description, enrollment } from './ActivityCard.scss';
 
-const ActivityCard = ({id, title, imageUrl, location, date, sponsor, customers}) => (
-  <article className={classNames('column is-three-quarters', container)}>
-    <Link to={`/activity/${id}`}>
-      <div className={image}>
+const imagePlaceHolder = 'http://placehold.it/150x200';
+
+const ActivityCard = ({
+  id, title, imageUrl = imagePlaceHolder, location, date, sponsor, customers,
+}) => (
+  <div className="box">
+    <article className={classNames('media', container)}>
+      <div className={classNames('media-left', image)}>
         <img src={imageUrl} alt={`${title}`} />
       </div>
-      <div className={description}>
+      <div className={classNames('media-right', description)}>
         <h3>{ title }</h3>
         <div className={styles.date}>{`时间: ${smartMoment(date, 'YYYY-MM-DD HH:mm:ss')}`}</div>
         <div className={styles.location}>{`地点: ${location}`}</div>
         <div className={styles.sponsor}>{`发起: ${sponsor}`}</div>
         <div className={enrollment}>{`参加人数: ${customers.length}`}</div>
+        <div>
+          <Link to={`/activity/${id}`} className="button is-primary">详情</Link>
+        </div>
       </div>
-    </Link>
-  </article>
+    </article>
+  </div>
 );
 
 ActivityCard.propTypes = {
--- src/vendor/index.jsx
+++ src/vendor/index.jsx
@@ -1,1 +1,2 @@
-import styles from './index.scss';
+require('./index.scss');
+
